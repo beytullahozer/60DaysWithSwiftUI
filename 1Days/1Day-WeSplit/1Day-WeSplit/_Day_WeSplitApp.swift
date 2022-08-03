7f667b3d5311013551af45c16f21a5d69f3157ea
beytullahozer <beytullahhozer@gmail.com>
2022-08-03 19:53:05 +0300

    update

---
+++ 1Days/1Day-WeSplit/1Day-WeSplit/_Day_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Day_WeSplitApp.swift
+//  1Day-WeSplit
+//
+//  Created by Beytullah Özer on 3.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct _Day_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
